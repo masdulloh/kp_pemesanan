e76b2558b8854ceaf3e0817014e5aad7fb610d5b
masdulloh <ma089983@gmail.com>
2019-12-29 02:19:58 +0700

    View Product 29 Dec 2.19 AM

--- urlmyid/src/components/products/Products.vue
+++ urlmyid/src/components/products/Products.vue
@@ -2,18 +2,65 @@
     <div class="product">
         <h2>Product</h2>
         <router-link :to="{ name:'AddProduct' }"><button type="button" class="btn btn-success">+Add Product</button></router-link>
+        <p>&nbsp;</p>
+
+
+        <table class="table table-striped table-hover">
+            <thead>
+                <tr>
+                <th scope="col">#</th>
+                <th scope="col">Product Name</th>
+                <th scope="col">Last</th>
+                <th scope="col">Handle</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <!-- Perulangan menampilkan product dari database -->
+                <tr v-for="(product, index) in productdisplay" :key="index">
+                    <td></td>
+                    <td>{{ product.pname }}</td>
+                    <td>{{ product.pprice }}</td>
+                    <td></td>
+                </tr>
+                <!-- END -->
+
+            </tbody>
+        </table>
+
+
     </div>
 </template>
 
 <script>
+import db from '@/firebase/init'
 import firebase from 'firebase'
+
 export default {
     name: 'Products',
     data(){
         return{
-
+            pneme: null,
+            productdisplay:[],
+            pprice: null
         }
     },
+    created(){
+        //view product
+        db.collection('products')
+        .onSnapshot((snapshot) => {
+            snapshot.docChanges().forEach(change => {
+                if(change.type == 'added'){
+                    this.productdisplay.unshift({
+                        pname: change.doc.data().pname,
+                        pprice: change.doc.data().pprice
+                    })
+                }
+            })
+        })
+    },
+
+    // 
     mounted(){
         //munculkan user sekarang
         //console.log(firebase.auth().currentUser)
